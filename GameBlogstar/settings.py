9134f81d99b8b60d6231725cd0355b3e4b18db24
AnasEljandi <anas.eljandi.110@gmail.com>
2025-10-07 21:24:38 +0100

    Databases and deployment

--- GameBlogstar/settings.py
+++ GameBlogstar/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9z$115&e115n@7jap=adv22n74x!0ol61k@s@eptv&vsh8lkn_'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["gameblogfinal-5509218430c6.herokuapp.com",
     "localhost",
--- envy.py
+++ envy.py
@@ -2,3 +2,4 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "<your-database-URL>")
+os.environ.setdefault("SECRET_KEY", "<your_choice_of_secret_key>")
